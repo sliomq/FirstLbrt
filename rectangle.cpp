e103e3a0a4e8ed4356389f7ccff7d74e514e64f1
Never <mr.gotas@mail.ru>
2024-03-18 16:24:46 +0700

    I do this

---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int oneSd, twoSd;
+	cout << "Введите длины двух сторон прямоугольника:" << endl;
+	cin >> oneSd >> twoSd;
+	cout << "Периметр: " << (oneSd + twoSd) * 2 << endl;
+	cout << "Площадь: " << oneSd * twoSd << endl;
+	cout << "Длина каждой диагонали:" << pow(pow(oneSd, 2) + pow(twoSd, 2), 0.5) << endl;
+	return 0;
+}
